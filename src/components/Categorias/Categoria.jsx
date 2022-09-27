edd46d8e11b86937e2c90c9aff614a8fc24ad6c4
arieltecay <arieltecay@gmail.com>
2022-09-27 19:47:11 -0300

    update clase vivo

--- src/components/Categorias/Categoria.jsx
+++ src/components/Categorias/Categoria.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BorderDecoration, CardCategoria } from './CategoriasStyles';
 
 export const Categoria = ({ id, img, title, category }) => {
-  // console.log(id, img, title, category);
+  console.log(id, img, title, category);
   
   return (
     <CardCategoria
